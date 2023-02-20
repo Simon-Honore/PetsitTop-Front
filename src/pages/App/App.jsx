6e445c46910bf6c9a8cdf1a0d083f3c0694b6b9c
Simon-Honore <simon.honore6@gmail.com>
2023-02-20 15:05:20 +0100

    build(app): directory architecture

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+  console.log('APP');
+
+  return (
+    <div className="App">
+      <header></header>
+			<main>Hello</main>
+			<footer></footer>
+    </div>
+  )
+}
+
+export default App
