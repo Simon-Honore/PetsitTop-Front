f64715a0be21c16e1c28a8cd646bb8470b501802
ines-dai <inesdai59@gmail.com>
2023-02-24 10:03:46 +0100

    feat(SearchResults) : added list of animals accepted to petsitter card

--- src/pages/SearchResults/PetsitterCard.jsx
+++ src/pages/SearchResults/PetsitterCard.jsx
@@ -1,20 +1,27 @@
+import { MdPlace } from "react-icons/md"
+import './PetsitterCard.scss';
 
-function PetsitterCard({nom, prenom, ville, codePostal, description}) {
-    return (
-    <div >
-      <div>
-        <h1>
-          {prenom}
-          {nom}
+function PetsitterCard({
+  nom, prenom, ville, codePostal, description, animauxAcceptes
+}) {
+  return (
+    <div className="petsitter__card">
+      <div className="petsitter__card__header">
+        <h1 className="petsitter__card__title">
+          {`${prenom} ${Array.from(nom)[0]}.`}
         </h1>
-        <p>
-          {ville}
-          {codePostal}
+        <p className="petsitter__card__location">
+          <MdPlace/> {`${ville} - ${codePostal}`}
         </p>
       </div>
       <p>{description}</p>
+      <p> Je peux garder vos :</p>
+      {/* {console.log(animauxAcceptes)} */}
+      {animauxAcceptes.map((animal) => 
+      <p> {animal} </p>
+      )}
     </div>
-    )
+  );
 }
 
 export default PetsitterCard;
--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -6,25 +6,25 @@ import './SearchResults.scss';
 const petSitters = [
   {
     id: 1,
-    nom: 'dai',
-    prenom: 'ines',
+    nom: 'Dai',
+    prenom: 'Ines',
     code_postal: 59700,
     ville: 'Marcq-en-Baroeul',
     description: 'bonjour, je suis disponible pour garder vos chats en votre absence, je peux me déplacer si couverture des frais de déplacement',
+    animaux_acceptes: ['chat', 'chien', 'NAC', 'reptile', 'oiseau', 'poisson']
   },
   {
     id: 2,
-    nom: 'honoré',
-    prenom: 'simon',
+    nom: 'Honoré',
+    prenom: 'Simon',
     code_postal: 85300,
     ville: 'Challans',
     description: 'bonjour, je suis disponible pour garder votre chien pendant les vacances car mon chien aime avoir un copain à la maison',
+    animaux_acceptes: ['chien', 'NAC', 'reptile', 'oiseau', 'poisson']
   },
 ];
 
-
 function SearchResults() {
-
   return (
     <main className="main-home">
       <section className="search-petsitters">
@@ -42,16 +42,17 @@ function SearchResults() {
         </form>
       </section>
 
-      {petSitters.map((petsitter) => 
-        <PetsitterCard key={petsitter.id}
-        prenom={petsitter.prenom}
-        nom={petsitter.nom}
-        ville={petsitter.ville}
-        codePostal={petsitter.code_postal}
-        description={petsitter.description}
+      {petSitters.map((petsitter) => (
+        <PetsitterCard
+          key={petsitter.id}
+          prenom={petsitter.prenom}
+          nom={petsitter.nom}
+          ville={petsitter.ville}
+          codePostal={petsitter.code_postal}
+          description={petsitter.description}
+          animauxAcceptes={petsitter.animaux_acceptes}
         />
-        );
-        }
+      ))}
 
     </main>
   );
