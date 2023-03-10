7dc755550d6d997c77d586505510f5eee660bf2c
ines-dai <inesdai59@gmail.com>
2023-03-10 11:37:54 +0100

    fix(PetIcon): added an images directory in src for pet icons

--- src/components/PetIcon/PetIcon.jsx
+++ src/components/PetIcon/PetIcon.jsx
@@ -1,18 +1,47 @@
-// import oiseau from '../../assets/oiseau.svg';
-// import rongeur from '../../assets/rongeur.svg';
-// import chat from '../../assets/chat.svg';
-// import chien from '../../assets/chien.svg';
-// import poisson from '../../assets/poisson.svg';
-// import reptile from '../../assets/reptile.svg';
-// import lapin from '../../assets/lapin.svg';
+import oiseau from '../../images/oiseau.svg';
+import rongeur from '../../images/rongeur.svg';
+import chat from '../../images/chat.svg';
+import chien from '../../images/chien.svg';
+import poisson from '../../images/poisson.svg';
+import reptile from '../../images/reptile.svg';
+import lapin from '../../images/lapin.svg';
+import autre from '../../images/autre.svg';
 import './PetIcon.scss';
 
 function PetIcon({ pet }) {
+  let petIcon;
+  switch (pet) {
+    case 'oiseau':
+      petIcon = oiseau;
+      break;
+    case 'rongeur':
+      petIcon = rongeur;
+      break;
+    case 'chat':
+      petIcon = chat;
+      break;
+    case 'chien':
+      petIcon = chien;
+      break;
+    case 'poisson':
+      petIcon = poisson;
+      break;
+    case 'reptile':
+      petIcon = reptile;
+      break;
+    case 'lapin':
+      petIcon = lapin;
+      break;
+    case 'autre':
+      petIcon = autre;
+    default:
+      break;
+  }
   return (
     <div className="peticon">
       <img
         className="peticon__img"
-        src={`/${pet}.svg`}
+        src={petIcon}
         alt={`Icône ${pet}`}
       />
       <p className="peticon__title">
