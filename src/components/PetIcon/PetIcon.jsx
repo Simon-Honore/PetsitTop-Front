3f2b79792027315231b5574c33cef674d2c703a1
Simon-Honore <simon.honore6@gmail.com>
2023-03-10 10:22:27 +0100

    fix(app) import icon pet

--- src/components/PetIcon/PetIcon.jsx
+++ src/components/PetIcon/PetIcon.jsx
@@ -1,10 +1,10 @@
-import oiseau from '../../assets/oiseau.svg';
-import rongeur from '../../assets/rongeur.svg';
-import chat from '../../assets/chat.svg';
-import chien from '../../assets/chien.svg';
-import poisson from '../../assets/poisson.svg';
-import reptile from '../../assets/reptile.svg';
-import lapin from '../../assets/lapin.svg';
+// import oiseau from '../../assets/oiseau.svg';
+// import rongeur from '../../assets/rongeur.svg';
+// import chat from '../../assets/chat.svg';
+// import chien from '../../assets/chien.svg';
+// import poisson from '../../assets/poisson.svg';
+// import reptile from '../../assets/reptile.svg';
+// import lapin from '../../assets/lapin.svg';
 import './PetIcon.scss';
 
 function PetIcon({ pet }) {
@@ -12,7 +12,7 @@ function PetIcon({ pet }) {
     <div className="peticon">
       <img
         className="peticon__img"
-        src={`/src/assets/${pet}.svg`}
+        src={`/${pet}.svg`}
         alt={`Icône ${pet}`}
       />
       <p className="peticon__title">
