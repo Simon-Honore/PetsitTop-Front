328d3508b0f21a51d7b132360c13bf7141c4c430
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-03-10 19:30:36 +0100

    style: add style for pet

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -1,7 +1,18 @@
-import './PetCard.scss';
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import { BsPencilSquare } from 'react-icons/bs';
 import PetIcon from '../PetIcon/PetIcon';
 
+import './PetCard.scss';
+
 function PetCard({ name, pet_type, presentation }) {
+  const [showUpdatePet, setShowUpdatePet] = useState(false);
+
+  const openUpdateForm = () => {
+    setShowUpdatePet(!showUpdatePet);
+    console.log('clicked');
+  };
+
   return (
     <div className="profile__pets__item">
       <div className="profile__pets__item__top">
@@ -17,9 +28,9 @@ function PetCard({ name, pet_type, presentation }) {
           </div>
         </div>
 
-        <div>
-          {/* <BsPencilSquare onClick={openUpdateForm} />
-          <FaTrashAlt /> */}
+        <div className="profile__pets__item__btns">
+          <BsPencilSquare onClick={openUpdateForm} />
+          <FaTrashAlt />
         </div>
       </div>
       <div className="profile__pets__item__description">
--- src/pages/MyProfile/PetProfile/PetProfile.jsx
+++ src/pages/MyProfile/PetProfile/PetProfile.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { FaUserCircle, FaTrashAlt } from 'react-icons/fa';
-import { BsPencilSquare } from 'react-icons/bs';
 import { FiPlus } from 'react-icons/fi';
 import AddPet from './AddPet/AddPet';
 import UpdatePet from './UpdatePet/UpdatePet';
@@ -8,7 +6,6 @@ import PetCard from '../../../components/PetCard/PetCard';
 import './PetProfile.scss';
 
 function PetProfile({ pets }) {
-  const [showUpdatePet, setShowUpdatePet] = useState(false);
 
   const [addPet, setAddPet] = useState(false);
 
@@ -19,16 +16,11 @@ function PetProfile({ pets }) {
     console.log('it works');
   };
 
-  const openUpdateForm = () => {
-    setShowUpdatePet(!showUpdatePet);
-    console.log('clicked');
-  };
-
   return (
     <div className="profile__pets">
       <div className="profile__pets__header">
         <h1 className="profile__pets__header__title">Mes animaux</h1>
-        <button type="button" onClick={() => { switchState(addPet, setAddPet); }}>
+        <button type="button" className="profile__pets__header__add" onClick={() => { switchState(addPet, setAddPet); }}>
           <FiPlus />
           Ajouter
         </button>
@@ -38,19 +30,21 @@ function PetProfile({ pets }) {
           <AddPet />
         ) : ''}
 
-      { showUpdatePet
+      {/* { showUpdatePet
         ? (
           <UpdatePet />
-        ) : ''}
+        ) : ''} */}
 
-      { pets && pets.map((pet) => (
-        <PetCard
-          key={pet.id}
-          name={pet.name}
-          pet_type={pet.pet_type}
-          presentation={pet.presentation}
-        />
-      ))}
+      <div class="profile__pets__items">
+        { pets && pets.map((pet) => (
+          <PetCard
+            key={pet.id}
+            name={pet.name}
+            pet_type={pet.pet_type}
+            presentation={pet.presentation}
+          />
+        ))}
+      </div>
     </div>
   );
 }
