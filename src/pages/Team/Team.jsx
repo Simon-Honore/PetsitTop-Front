00fcf80ba210487eeea49dd9b2b1ef6d807a8cd1
ines-dai <inesdai59@gmail.com>
2023-03-11 17:25:56 +0100

    style(Team): box sizes, colors and descriptions

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -6,14 +6,19 @@ import './Team.scss';
 function Team() {
   return (
     <main className="team">
-      <h1>Équipe</h1>
-      <p>Le site petsitTop a été développé dans le cadre du projet d'Apothéose de la formation O'Clock "Développeur Fullstack JS".</p>
+      <div className="team__header">
+        <h1>Équipe</h1>
+        <p>Le site petsitTop a été développé dans le cadre du projet d'Apothéose de la formation O'Clock "Développeur Fullstack JS".</p>
+      </div>
       <div className="team__members">
         <div className="team__member">
           <img src="https://avatars.githubusercontent.com/u/113342126?v=4" alt="Simon" />
           <h2>Simon Honoré</h2>
-          <h3>Project Owner <br />& Lead Dev Front</h3>
-          <p>Brève description Simon</p>
+          <h3>Product Owner <br />& Lead Dev Front</h3>
+          <p>Simon n'a rien à envier aux autres lead devs : compétent, pédagogue, patient, pro du débugage, charmeur de reducers...</p>
+          <p>Malheureusement pour vous, il est déjà marié.<br />
+            Heureusement pour vous, il est ouvert aux opportunités sur Challans (85) et alentours ou en fullremote. 😉
+          </p>
           <div className="team__member__links">
             <a href="https://www.linkedin.com/in/simon-honore/" target="_blank" rel="noreferrer">Linkedin</a>
           </div>
@@ -21,8 +26,8 @@ function Team() {
         <div className="team__member">
           <img src="https://avatars.githubusercontent.com/u/111069852?v=4" alt="Ines" />
           <h2>Ines Dai</h2>
-          <h3>Scrum Master</h3>
-          <p>Description</p>
+          <h3>Scrum Master <br /> & Dev Front</h3>
+          <p></p>
           <div className="team__member__links">
             <a href="https://www.linkedin.com/in/ines-a-d/" target="_blank" rel="noreferrer">Linkedin</a>
           </div>
@@ -30,8 +35,12 @@ function Team() {
         <div className="team__member">
           <img src="https://avatars.githubusercontent.com/u/113342270?v=4" alt="Axelle" />
           <h2>Axelle Skica</h2>
-          <h3>Git Master</h3>
-          <p>Description</p>
+          <h3>Git Master <br /> & Dev Back</h3>
+          <p>Etoile montante du dev backend, Axelle a de nombreuses cordes à son arc : marketing digital, crochet et yoga pour n'en citer que 3.</p>
+          <p>S'il fallait toutes les lister, il faudrait probablement les stocker dans une base de données. <br />
+            Ca tombe bien : c'est sa spécialité.
+          </p>
+          <p>A l'écoute d'opportunités sur Clermont-Ferrand (63) et sa périphérie, dans la limite des pistes cyclables. 🚴</p>
           <div className="team__member__links">
             <a href="https://www.linkedin.com/in/axelle-skica/" target="_blank" rel="noreferrer">Linkedin</a>
           </div>
@@ -39,8 +48,9 @@ function Team() {
         <div className="team__member">
           <img src="https://avatars.githubusercontent.com/u/113336393?v=4" alt="Catherine" />
           <h2>Catherine Keller</h2>
-          <h3>Lead Dev Back</h3>
-          <p>Description</p>
+          <h3>Lead Dev Back <br /> & renfort Front</h3>
+          <p>Le couteau suisse de l'équipe : graphiste, intégratrice, chargée de communication, dev back, dev front... <br /> Catherine manie le web comme elle manie la harpe 🎵</p>
+          <p>Disponible pour travailler ou pour danser sur Bischoffsheim (67) et alentours ou en fullremote.</p>
           <div className="team__member__links">
             <a href="https://www.linkedin.com/in/kellercatherine/" target="_blank" rel="noreferrer">Linkedin</a>
           </div>
